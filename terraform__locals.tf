5bfe78d2b56a19825b51ad1d0a7beebb17df23d8
tannevaled <tannevaled@users.noreply.github.com>
2024-10-24 15:35:55 +0200

    wip

--- terraform__locals.tf
+++ terraform__locals.tf
@@ -1,6 +1,6 @@
 locals {
-    deployment = var.deployment
-    inventory_file_path = var.inventory_file_path
+    deployment = var.get-volumes.deployment
+    inventory_file_path = var.get-volumes.inventory_file_path
     cluster_prefix = var.cluster_prefix
     group_prefix = var.group_prefix
     volume_name = var.volume_name
