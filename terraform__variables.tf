9af8381e0c8f189232b73fed66de5befed6fd097
tannevaled <tannevaled@users.noreply.github.com>
2024-10-24 14:06:48 +0200

    wip

--- terraform__variables.tf
+++ terraform__variables.tf
@@ -1,3 +1,15 @@
+variable get-volumes {
+    type = object({
+        deployment = object({
+            id = string
+            uuid = string
+            tenant_name = string
+            domain_name = string
+        })
+        inventory_file_path = string
+    })
+}
+
 variable deployment {
     type = object({
       id = string
